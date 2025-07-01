7d15ba8a09c7455b438462af6ba155b4c67b4875
Sergiocesani <sdcesani@gmail.com>
2025-07-01 14:04:31 -0300

    probamos una version mas segura

--- src/barcode.js
+++ src/barcode.js
@@ -2,10 +2,15 @@ export function renderBarcode(code) {
   // Limpiar código (eliminar espacios)
   code = code.trim();
 
-  let format = "CODE128";
+let format;
 
-  if (code.length === 13) format = "EAN13";
-  else if (code.length === 8) format = "EAN8";
+if (/^\d{13}$/.test(code)) {
+  format = "EAN13";
+} else if (/^\d{8}$/.test(code)) {
+  format = "EAN8";
+} else {
+  format = "CODE128";
+}
 
   JsBarcode("#barcode", code, {
     format: format,
