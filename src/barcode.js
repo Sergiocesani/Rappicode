abce016d3f18b695a15ba4d624f147af9efcbb14
SergioCesani <sdcesani@gmail.com>
2026-05-02 14:38:18 -0300

    Volvemos con correciones css

--- src/barcode.js
+++ src/barcode.js
@@ -9,13 +9,12 @@ export function renderBarcode(code) {
     JsBarcode("#barcode", clean, {
       format,
       lineColor: "rgba(3, 3, 3, 1)",
-      // --- CAMBIOS AQUÍ ---
-      width: 1.5,        // Bajamos de 2 a 1.5 para que sea más angosto
-      height: 70,       // Bajamos un poco la altura (estaba en 80)
-      displayValue: true,
-      fontSize: 14,      // Tamaño de fuente más limpio
-      margin: 10,
-      // --------------------
+      width: 1.7,           // Grosor optimizado para que no se desborde
+      height: 70,          // Altura balanceada
+      displayValue: true,   // Muestra los números abajo
+      fontSize: 15,         // Tamaño de números legible
+      margin: 10,           // Espacio interno
+      background: "#ffffff" // Fondo blanco puro para contraste
     });
   } catch (err) {
     console.error("🚫 Error al renderizar:", err);
